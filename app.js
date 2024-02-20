cace6351b4a3066c52ec72e177e7ca78851d367a
GaborSzaboHUN <66555347+GaborSzaboHUN@users.noreply.github.com>
2024-02-20 23:53:54 +0100

    Button click events done

--- app.js
+++ app.js
@@ -7,5 +7,54 @@ const nextSequence = () => {
 
 const randomChosenColor = () => {
     gamePattern.push(buttonColors[nextSequence()])
-    console.log(gamePattern)
+    return (buttonColors[nextSequence()])
+}
+
+// - - - - - - - - Button click event listener 
+
+const buttons = $('[type="button"]')
+
+buttons.click((e) => {
+    const buttonId = e.target.id
+    handleClick(buttonId)
+    clickAnimation($(e.target))
+})
+
+// - - - - - - - - Button click animation 
+
+const clickAnimation = (button) => {
+    button.addClass('pressed')
+
+    setTimeout(() => {
+        button.removeClass('pressed')
+    }, 200);
+}
+
+// - - - - - - - - Play song according to click - SWITCH
+
+const handleClick = (key) => {
+    switch (key) {
+        case 'blue':
+            const blue = new Audio('sounds/blue.mp3')
+            blue.play()
+            break;
+
+        case 'green':
+            const green = new Audio('sounds/green.mp3')
+            green.play()
+            break;
+
+        case 'red':
+            const red = new Audio('sounds/red.mp3')
+            red.play()
+            break;
+
+        case 'yellow':
+            const yellow = new Audio('sounds/yellow.mp3')
+            yellow.play()
+            break;
+
+        default: console.log(key)
+            break;
+    }
 }
